afcbb808c2f51194dc295ac59eb6e60520464865
BOSON-18 <deeshankbatra663@gmail.com>
2024-08-30 21:44:29 +0530

    dummy1

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+"use client"
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 
 
 export default function Home() {
+  const router=useRouter();
+  router.push('/dashboard')
   return (
    <div>
     <h1>Hello</h1>
