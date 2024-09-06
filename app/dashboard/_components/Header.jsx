62ab64007104fec0456f64b08954cabdc0959e6a
BOSON-18 <deeshankbatra663@gmail.com>
2024-09-06 17:45:24 +0530

    Link added

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -1,6 +1,7 @@
 "use client"
 import { UserButton } from "@clerk/nextjs"
 import Image from "next/image"
+import Link from "next/link"
 import { usePathname } from "next/navigation"
 import React, { useEffect } from "react"
 
@@ -15,7 +16,9 @@ const Header = () => {
     <div className="flex p-4 items-center justify-between bg-secondary shadow-sm">
       <Image src={"/logo.svg"} alt="logo" width={160} height={100}></Image>
       <ul className="hidden md:flex gap-6 "> {/**small pr hiedden and md lg pr show hoga */}
+        <Link href={"/dashboard"} >
         <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=="/dashboard" && "text-primary font-bold"}`}>Dashboard</li>
+        </Link>
         <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=="/dashboard/questions" && "text-primary font-bold"}`} >Questions</li>
         <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=="/dashboard/upgrade" && "text-primary font-bold"}`} >Upgrade</li>
         <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=="/dashboard/work" && "text-primary font-bold"}`} >How it works ?</li>
